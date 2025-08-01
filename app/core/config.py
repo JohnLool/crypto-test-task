ded8593a5169b22eefe446e7e745d30ab2dd7f8c
JohnLool <pinaev-misha-pinaev@yandex.ru>
2025-08-01 07:06:52 +0500

    feat: add api key for tron client

--- app/core/config.py
+++ app/core/config.py
@@ -8,6 +8,8 @@ class Settings(BaseSettings):
     DB_PASS: str
     DB_NAME: str
 
+    API_KEY: str
+
     @property
     def database_url(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
--- app/utils/tron.py
+++ app/utils/tron.py
@@ -1,10 +1,15 @@
 from tronpy import AsyncTron
+from tronpy.providers import AsyncHTTPProvider
 from tronpy.exceptions import AddressNotFound
 from .logger import logger
+from app.core.config import settings
+
+
+provider = AsyncHTTPProvider(api_key=settings.API_KEY)
 
 
 async def get_wallet_info(address: str) -> dict:
-    async with AsyncTron(network="mainnet") as client:
+    async with AsyncTron(network="mainnet", provider=provider) as client:
         try:
             bandwidth = await client.get_bandwidth(address)
             balance = await client.get_account_balance(address)
